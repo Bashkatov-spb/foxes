8c490b923134e65394448519a059163bff54a7a5
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-11-08 22:11:08 +0300

    dist

--- server/app.js
+++ server/app.js
@@ -10,10 +10,10 @@ const app = express();
 const indexRouter = require('./routes/index.routes');
 
 serverConfig(app);
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use('/', indexRouter);
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('../client/build/index.html'));
+  res.sendFile(path.resolve('../client/dist/index.html'));
 });
 const PORT = process.env.PORT || 3000;
 
